5808a29dc0ac51d906e7d4f0ebf2b1be790f287c
Scott Clay <s.scott.clay@gmail.com>
2020-12-06 13:27:28 -0700

    add NATGW to private subnet rt

--- routetables.tf
+++ routetables.tf
@@ -26,23 +26,35 @@ resource "aws_route_table" "RTPublicB" {
 
 }
 
-# #Make route tables for the Private Subnets
-# #Empty for now
+#Make route tables for the Private Subnets
+#Where the NATGWs go
 
 resource "aws_route_table" "RTPrivateA" {
   vpc_id = aws_vpc.Hutch1.id
-
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.NATGWA.id
+  }
   tags = {
     Name = "RTPrivateA"
   }
+
+  depends_on = [aws_nat_gateway.NATGWA]
+
 }
 
 resource "aws_route_table" "RTPrivateB" {
   vpc_id = aws_vpc.Hutch1.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.NATGWB.id
+  }
 
   tags = {
     Name = "RTPrivateB"
   }
+
+  depends_on = [aws_nat_gateway.NATGWB]
 
 }
 
