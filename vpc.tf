0a6aa71f43298db382363ae5e78c81e80823eec6
Scott Clay <s.scott.clay@gmail.com>
2020-12-06 19:36:58 -0700

    combine vpc, igw

--- vpc.tf
+++ vpc.tf
@@ -5,3 +5,11 @@ resource "aws_vpc" "Hutch1" {
     Name = "hutch1"
   }
 }
+
+# #Create an Internet Gateway for the Public Route Tables
+resource "aws_internet_gateway" "IGW" {
+  vpc_id = aws_vpc.Hutch1.id
+  tags = {
+    Name = "Hutch1IGW"
+  }
+}
